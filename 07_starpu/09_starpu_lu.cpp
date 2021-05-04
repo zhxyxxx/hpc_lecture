a15552275e911c546e7546284dc0cc99f61f6be0
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2021-05-04 22:22:43 +0900

    TRSM and GEMM on StarPU

--- 07_starpu/09_starpu_lu.cpp
+++ 07_starpu/09_starpu_lu.cpp
@@ -16,10 +16,46 @@ void getrf(void *buffers[], void *) {
   LAPACKE_dgetrf(LAPACK_ROW_MAJOR, N, N, A, N, ipiv.data());
 }
 
+struct starpu_codelet getrf_cl = {
+  .cpu_funcs = {getrf},
+  .nbuffers = 1,
+};
+
+void trsm(void *buffers[], void *cl_arg) {
+  double *A = (double *)STARPU_MATRIX_GET_PTR(buffers[0]);
+  double *B = (double *)STARPU_MATRIX_GET_PTR(buffers[1]);
+  int NA = (int)STARPU_MATRIX_GET_NX(buffers[0]);
+  int NB = (int)STARPU_MATRIX_GET_NY(buffers[1]);
+  bool left, up;
+  starpu_codelet_unpack_args(cl_arg, &left, &up);
+  cblas_dtrsm(CblasRowMajor, left ? CblasLeft : CblasRight, up ? CblasUpper : CblasLower,
+              CblasNoTrans, up ? CblasNonUnit : CblasUnit, NA, NB, 1.0, A, NA, B, NB);
+}
+
+struct starpu_codelet trsm_cl = {
+  .cpu_funcs = {trsm},
+  .nbuffers = 2,
+};
+
 void trsm(bool left, bool up, vector<double>& A, int NA, vector<double>& B, int NB) {
   cblas_dtrsm(CblasRowMajor, left ? CblasLeft : CblasRight, up ? CblasUpper : CblasLower,
               CblasNoTrans, up ? CblasNonUnit : CblasUnit, NA, NB, 1.0, A.data(), NA, B.data(), NB);
 }
+
+void gemm(void *buffers[], void *) {
+  double *A = (double *)STARPU_MATRIX_GET_PTR(buffers[0]);
+  double *B = (double *)STARPU_MATRIX_GET_PTR(buffers[1]);
+  double *C = (double *)STARPU_MATRIX_GET_PTR(buffers[2]);
+  int NA = (int)STARPU_MATRIX_GET_NX(buffers[0]);
+  int NB = (int)STARPU_MATRIX_GET_NY(buffers[1]);
+  cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, NA, NB, NA, -1.0,
+              A, NA, B, NB, 1.0, C, NB);
+}
+
+struct starpu_codelet gemm_cl = {
+  .cpu_funcs = {gemm},
+  .nbuffers = 3,
+};
 
 void gemm(vector<double>& A, int NA, vector<double>& B, int NB, vector<double>& C) {
   cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, NA, NB, NA, -1.0,
@@ -45,7 +81,7 @@ int main() {
   for (int m=0; m<M; m++) {
     x[m] = vector<double>(N);
     b[m] = vector<double>(N);
-    starpu_vector_data_register(&b_h[m],0,(uintptr_t)b[m].data(),N,sizeof(double));
+    starpu_matrix_data_register(&b_h[m],0,(uintptr_t)b[m].data(),N,1,1,sizeof(double));
   }
   for (int m=0; m<M; m++) {
     for (int i=0; i<N; i++) {
@@ -63,22 +99,21 @@ int main() {
       }
     }
   }
-  starpu_codelet getrf_cl;
-  starpu_codelet_init(&getrf_cl);
-  getrf_cl.cpu_funcs[0] = getrf;
-  getrf_cl.nbuffers = 1;
-  getrf_cl.modes[0] = STARPU_RW;
   for (int l=0; l<M; l++) {
     starpu_task_insert(&getrf_cl, STARPU_RW, A_h[M*l+l], 0);
-    starpu_task_wait_for_all();
     for (int m=l+1; m<M; m++) {
-      trsm(left, lower, A[M*l+l], N, A[M*l+m], N);
-      trsm(right, upper, A[M*l+l], N, A[M*m+l], N);
+      starpu_task_insert(&trsm_cl, STARPU_RW, A_h[M*l+l], STARPU_RW, A_h[M*l+m], 
+			 STARPU_VALUE, &left, sizeof(bool),
+			 STARPU_VALUE, &lower, sizeof(bool), 0);
+      starpu_task_insert(&trsm_cl, STARPU_RW, A_h[M*l+l], STARPU_RW, A_h[M*m+l], 
+			 STARPU_VALUE, &right, sizeof(bool),
+			 STARPU_VALUE, &upper, sizeof(bool), 0);
     }
     for (int m=l+1; m<M; m++)
       for (int n=l+1; n<M; n++)
-        gemm(A[M*m+l], N, A[M*l+n], N, A[M*m+n]);
+	starpu_task_insert(&gemm_cl, STARPU_RW, A_h[M*m+l], STARPU_RW, A_h[M*l+n], STARPU_RW, A_h[M*m+n], 0);
   }
+  starpu_task_wait_for_all();
   for (int m=0; m<M; m++) {
     for (int n=0; n<m; n++)
       gemm(A[M*m+n], N, b[n], 1, b[m]);
