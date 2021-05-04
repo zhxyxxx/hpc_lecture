2d2e9f8dbdbe0905dbcc807740cc789956e98fc1
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2021-05-04 21:21:27 +0900

    Propagate convenience function

--- 07_starpu/06_2x2_lu.cpp
+++ 07_starpu/06_2x2_lu.cpp
@@ -8,8 +8,24 @@
 
 using namespace std;
 
+void getrf(vector<double>& A, int N) {
+  vector<int> ipiv(N);
+  LAPACKE_dgetrf(LAPACK_ROW_MAJOR, N, N, A.data(), N, ipiv.data());
+}
+
+void trsm(bool left, bool up, vector<double>& A, int NA, vector<double>& B, int NB) {
+  cblas_dtrsm(CblasRowMajor, left ? CblasLeft : CblasRight, up ? CblasUpper : CblasLower,
+              CblasNoTrans, up ? CblasNonUnit : CblasUnit, NA, NB, 1.0, A.data(), NA, B.data(), NB);
+}
+
+void gemm(vector<double>& A, int NA, vector<double>& B, int NB, vector<double>& C) {
+  cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, NA, NB, NA, -1.0,
+              A.data(), NA, B.data(), NB, 1.0, C.data(), NB);
+}
+
 int main() {
   int N = 16;
+  bool left = true, right = false, upper = true, lower = false;
   vector<double> A00(N*N), A01(N*N), A10(N*N), A11(N*N);
   vector<double> x0(N), x1(N);
   vector<double> b0(N), b1(N);
@@ -30,27 +46,27 @@ int main() {
     }
   }
   // L00,U00 = lu(A00) (overwrite A00)
-  LAPACKE_dgetrf(LAPACK_ROW_MAJOR, N, N, A00.data(), N, ipiv.data());
+  getrf(A00, N);
   // U01 = L00^-1 * A01 (overwrite A01)
-  cblas_dtrsm(CblasRowMajor, CblasLeft, CblasLower, CblasNoTrans, CblasUnit, N, N, 1.0, A00.data(), N, A01.data(), N);
+  trsm(left, lower, A00, N, A01, N);
   // L10 = A01 * U00^-1 (overwrite A10)
-  cblas_dtrsm(CblasRowMajor, CblasRight, CblasUpper, CblasNoTrans, CblasNonUnit, N, N, 1.0, A00.data(), N, A10.data(), N);
+  trsm(right, upper, A00, N, A10, N);
   // A11 -= L10 * U01 (overwrite A11)
-  cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, N, N, N, -1.0, A10.data(), N, A01.data(), N, 1.0, A11.data(),N);
+  gemm(A10, N, A01, N, A11);
   // L11,U11 = lu(A11) (overwrite A11)
-  LAPACKE_dgetrf(LAPACK_ROW_MAJOR, N, N, A11.data(), N, ipiv.data());
+  getrf(A11, N);
   // y0 = L00^-1 b0 (overwrite b0)
-  cblas_dtrsm(CblasRowMajor, CblasLeft, CblasLower, CblasNoTrans, CblasUnit, N, 1, 1.0, A00.data(), N, b0.data(), 1);
+  trsm(left, lower, A00, N, b0, 1);
   // b1 -= L10 * y0 (overwrite b1)
-  cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, N, 1, N, -1.0, A10.data(), N, b0.data(), 1, 1.0, b1.data(), 1);
+  gemm(A10, N, b0, 1, b1);
   // y1 = L11^-1 * b1 (overwrite b1)
-  cblas_dtrsm(CblasRowMajor, CblasLeft, CblasLower, CblasNoTrans, CblasUnit, N, 1, 1.0, A11.data(), N, b1.data(), 1);
+  trsm(left, lower, A11, N, b1, 1);
   // x1 = U11^-1 * y1 (overwrite b1)
-  cblas_dtrsm(CblasRowMajor, CblasLeft, CblasUpper, CblasNoTrans, CblasNonUnit, N, 1, 1.0, A11.data(), N, b1.data(), 1);
+  trsm(left, upper, A11, N, b1, 1);
   // y0 -= U01 * x1 (overwrite b0)
-  cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, N, 1, N, -1.0, A01.data(), N, b1.data(), 1, 1.0, b0.data(),1);
+  gemm(A01, N, b1, 1, b0);
   // x0 = U00^-1 * y0 (overwrite b1)
-  cblas_dtrsm(CblasRowMajor, CblasLeft, CblasUpper, CblasNoTrans, CblasNonUnit, N, 1, 1.0, A00.data(), N, b0.data(), 1);
+  trsm(left, upper, A00, N, b0, 1);
 
   double diff = 0, norm = 0;
   for (int i=0; i<N; i++) {
